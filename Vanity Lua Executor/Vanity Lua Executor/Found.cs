b82ed5e3dc6c12a98934621ddd5dc4f5dce018e7
VanityYT <75121692+VanityYT@users.noreply.github.com>
2020-11-27 06:44:43 -0500

    Add files via upload

---
+++ Vanity Lua Executor/Vanity Lua Executor/Found.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Vanity_Lua_Executor
+{
+	internal class Found
+	{
+		[DllImport("User32.dll", CharSet = CharSet.Unicode)]
+		public static extern int MessageBox(IntPtr h, string m, string c, int type);
+
+		public static void uigasvdsajsa()
+		{
+			string path = "C:\\\\VanityPremium";
+			bool flag = Directory.Exists(path);
+			if (!flag)
+			{
+				Directory.CreateDirectory("C:\\VanityPremium");
+			}
+			bool flag2 = File.Exists("C:\\VanityPremium\\Enjoy.lua");
+			if (!flag2)
+			{
+				File.Create("C:\\VanityPremium\\Enjoy.lua");
+			}
+			Console.ForegroundColor = ConsoleColor.DarkMagenta;
+			Console.WriteLine("Put your code in C:\\VanityPremium\\Enjoy.lua");
+			Console.Write("Type Inject and press enter to inject");
+			Console.WriteLine(" WARNING YOU'RE GAME WILL CRASH DO NOT EXECUTE YET!!");
+			Console.WriteLine("");
+			for (; ; )
+			{
+				string a = Console.ReadLine();
+				bool flag3 = a == "Inject";
+				if (flag3)
+				{
+					Found.Execute();
+				}
+				bool flag4 = a == "inject";
+				if (flag4)
+				{
+					Found.Execute();
+				}
+			}
+		}
+
+		public static void Execute()
+		{
+			using (StreamWriter streamWriter = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\FiveM\\FiveM.app\\citizen\\scripting\\lua\\Sheduler.lua", true))
+			{
+				streamWriter.WriteLine("");
+			}
+			Found.VanityPremium();
+			Console.ReadLine();
+		}
+
+		public static void VanityPremium()
+		{
+			using (StreamWriter streamWriter = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\FiveM\\FiveM.app\\citizen\\scripting\\lua\\Sheduler.lua", true))
+			{
+				streamWriter.WriteLine("if GetCurrentResourceName() == \"spawnmanager\" then");
+			}
+			Found.VanityPremium2();
+			Console.ReadLine();
+		}
+
+		public static void VanityPremium2()
+		{
+			string text = "C:\\VanityPremium\\Enjoy.lua";
+			StreamReader streamReader = new StreamReader(text);
+			FileInfo fileInfo = new FileInfo(text);
+			StreamReader streamReader2 = fileInfo.OpenText();
+			string value;
+			while ((value = streamReader2.ReadLine()) != null)
+			{
+				using (StreamWriter streamWriter = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\FiveM\\FiveM.app\\citizen\\scripting\\lua\\Sheduler.lua", true))
+				{
+					streamWriter.WriteLine(value);
+				}
+			}
+			Found.VanityPremium3();
+			Console.ReadLine();
+		}
+
+		public static void VanityPremium3()
+		{
+			using (StreamWriter streamWriter = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\FiveM\\FiveM.app\\citizen\\scripting\\lua\\Sheduler.lua", true))
+			{
+				streamWriter.WriteLine("end");
+			}
+			Found.VanityPremium4();
+			Console.ReadLine();
+		}
+
+		// Token: 0x06000007 RID: 7 RVA: 0x000022A8 File Offset: 0x000004A8
+		public static void VanityPremium4()
+		{
+			using (StreamWriter streamWriter = new StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\AppData\\Local\\FiveM\\FiveM.app\\citizen\\scripting\\lua\\Sheduler.lua", true))
+			{
+				streamWriter.WriteLine("");
+			}
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("Injected!");
+			Thread.Sleep(5000);
+		}
+	}
+}
---
+++ Vanity Lua Executor/Vanity Lua Executor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Vanity_Lua_Executor
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.Title = "Vanity Premium FiveM Lua Exec";
+			Console.ForegroundColor = ConsoleColor.Blue;
+			string processName = "FiveM";
+			bool flag = Process.GetProcessesByName(processName).Length != 0;
+			if (flag)
+			{
+				Console.WriteLine("Found FiveM");
+				Thread.Sleep(3000);
+				Console.Clear();
+				Found.uigasvdsajsa();
+				Console.ReadLine();
+			}
+			else
+			{
+				Console.Write("Waiting for FiveM.");
+				Thread.Sleep(3000);
+			}
+			for (; ; )
+			{
+				string processName2 = "FiveM";
+				bool flag2 = Process.GetProcessesByName(processName2).Length != 0;
+				if (flag2)
+				{
+					break;
+				}
+				Console.Write(".");
+				Thread.Sleep(1000);
+			}
+			Console.WriteLine("Found FiveM!");
+			Thread.Sleep(3000);
+			Console.Clear();
+			Found.uigasvdsajsa();
+			Console.ReadLine();
+			Console.ReadLine();
+		}
+	}
+}
